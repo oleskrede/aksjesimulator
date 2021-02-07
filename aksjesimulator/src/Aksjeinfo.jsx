e9f840743bf7d64615a9962a4b1e1402128d95d1
Ole Steinar L. Skrede <ole.skrede+github@pm.me>
2021-02-07 14:05:02 +0100

    initial commit

---
+++ aksjesimulator/src/Aksjeinfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Aksjeinfo extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p>NEL</p>
+            </div>
+        );
+    }
+}
+
+export default Aksjeinfo;
---
+++ aksjesimulator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Aksjeinfo from './Aksjeinfo'
+
+function App() {
+  return (
+    <div className="App">
+      <Aksjeinfo />
+    </div>
+  );
+}
+
+export default App;
