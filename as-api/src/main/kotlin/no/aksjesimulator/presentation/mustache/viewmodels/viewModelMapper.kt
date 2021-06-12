337417e076b39c24b7f0abdb010f7469e359adbd
Ole Steinar L. Skrede <ole.skrede+github@pm.me>
2021-06-12 18:29:55 +0200

    typo in user spec account -> accounts

--- as-api/src/main/kotlin/no/aksjesimulator/presentation/mustache/viewmodels/viewModelMapper.kt
+++ as-api/src/main/kotlin/no/aksjesimulator/presentation/mustache/viewmodels/viewModelMapper.kt
@@ -1,11 +1,21 @@
 package no.aksjesimulator.presentation.mustache.viewmodels
 
 import no.aksjesimulator.domain.models.Account
+import no.aksjesimulator.domain.models.User
+
+data class AccountVM(val id: Int, val name: String, val balance: Int)
+
+data class UserVM(val username: String, val accounts: List<AccountVM>)
 
 fun accountTOAccountVm(account: Account): AccountVM {
     return AccountVM(
         account.id,
         account.name,
-        (account.balance / 100).toInt()
+        (account.balance).toInt()
     )
+}
+
+fun userToUserVM(user: User): UserVM {
+    val accountVMs = user.accounts?.map { accountTOAccountVm(it) } ?: emptyList()
+    return UserVM(user.username, accountVMs)
 }
